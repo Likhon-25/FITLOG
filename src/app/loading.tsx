1956a55b04550c90c8c14ddde839f13e93bc75ec
Likhon-25 <likhonmondol327@gmail.com>
2026-09-25 15:48:52 +0600

    add loading fallback

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="text-center m-10">
+      <span className="loading loading-spinner text-success"></span>
+    </div>
+  );
+};
+
+export default loading;
